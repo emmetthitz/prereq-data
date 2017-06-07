c63b3b6e4a320143100f46dbedc514b942933009
Emmett Hitz <emmetthitz@gmail.com>
2017-06-07 16:17:46 -0400

    maybe this time it works

--- app.rb
+++ app.rb
@@ -2,9 +2,7 @@ require 'sinatra'
 require 'sinatra/json'
 set :bind, '0.0.0.0'
 set :port, 3100
-configure do
-  enable :cross_origin
-end
+set :allow_origin, :any
 get '/cs/all' do
     content_type :json
     puts File.read('./public/prereq_data.json')
