b739096daa2eb68e7d14f53903ed04b0fcde892e
Emmett Hitz <emmetthitz@gmail.com>
2017-06-07 16:11:24 -0400

    cross-origin fix

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'sinatra/json'
 set :bind, '0.0.0.0'
 set :port, 3100
+configure do
+  enable :cross_origin
+end
 get '/cs/all' do
     content_type :json
     puts File.read('./public/prereq_data.json')
