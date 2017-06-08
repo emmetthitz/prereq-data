9e5a685b298c9f357725a18978523eea499650e4
rystills <rystills@gmail.com>
2017-06-08 14:36:52 -0400

    add sinatra-cross_origin and use in app.rb to enable CORS

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.4.1'
 gem 'rake'
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem "sinatra-cross_origin", "~> 0.3.1"
 gem 'nokogiri'
 gem 'activesupport'
 gem 'pry'
--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require 'sinatra/json'
 set :bind, '0.0.0.0'
 set :port, 3100
 set :allow_origin, :any
+configure do
+  enable :cross_origin
+end
 get '/cs/all' do
     content_type :json
     puts File.read('./public/prereq_data.json')
